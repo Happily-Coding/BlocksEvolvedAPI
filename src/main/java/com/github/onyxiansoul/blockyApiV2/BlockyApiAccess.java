9cda20614051bd811f9933e4f651b2fd0ed0e565
Uriel Salvio <salvio.uriel@gmail.com>
2021-02-23 14:59:08 -0300

    Early rework progress

--- src/main/java/com/github/onyxiansoul/blockyApiV2/BlockyApiAccess.java
+++ src/main/java/com/github/onyxiansoul/blockyApiV2/BlockyApiAccess.java
@@ -1,5 +1,15 @@
 package com.github.onyxiansoul.blockyApiV2;
 
-public class BlockyApiAccess {
+import me.onyxiansoul.blockyapi.BlockyAPI;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+/**The class which gives your plugin access to the BlockyAPI*/
+public interface BlockyApiAccess {
   
+  /**Get the API to get information about or extend the functionality of Blocky.*/
+  public BlockyAPI getBlockyAPI(@NotNull Plugin plugin);
+
 }
+  
+
